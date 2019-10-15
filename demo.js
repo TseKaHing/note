5a83d19259f28b4c2e2060cf25130f3bd55326b7
ThinkBig <1243756230@qq.com>
2019-10-15 14:05:56 +0800

    update

--- demo.js
+++ demo.js
@@ -1,21 +1,5 @@
-function FindNumsAppearOnce(array) {
-  // write code here
-  // return list, 比如[a,b]，其中ab是出现一次的两个数字
-  let list = []
-  for (let i = 0; i < array.length; i++) {
-    if (array.indexOf(i) == array.lastIndexOf(i)) {
-      console.log(array.indexOf(i), array.lastIndexOf(i));
-      list.push(array[i])
-    }
-  }
-  console.log(list);
-  if (list.length == 2) {
-    console.log(list);
-    return list
-  }
-
+for (var i = 0; i < 5; i++) {
+  console.log(i);
 }
 
-
-let arr = [1, 2, 2, 3, 3, 4]
-FindNumsAppearOnce(arr)
+console.log(i);
