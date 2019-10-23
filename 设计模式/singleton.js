013be7eb4468e928e9efd9b0304e435f72f6abc6
ThinkBig <1243756230@qq.com>
2019-10-23 12:45:56 +0800

    update

--- 设计模式/singleton.js
+++ 设计模式/singleton.js
@@ -1,7 +1,9 @@
+// 单例模式涉及到一个单一的类，该类负责创建自己的对象，同时确保只有单个对象被创建
+// 这个类提供了一种访问访问其唯一的对象的方式，可以直接访问，不需要实例化该类的对象
+
 var instance
 
 var foo = function () {
-
   if (!instance) {
     instance = new singleton()
   }
