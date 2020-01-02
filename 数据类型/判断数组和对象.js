6619e6935da25ed973304e9480ea36c7d6cfb77b
jazzyXie <1243756230@qq.com>
2020-01-02 21:47:46 +0800

    fix: :bug:添加更多判断数组的方法，添加NodeJS模块加载机制

--- 数据类型/判断数组和对象.js
+++ 数据类型/判断数组和对象.js
@@ -1,5 +1,12 @@
 // 通过 toString 打印可以判断是否为数组
 
-function isArray(value) {
-  return Object.prototype.toString.call(value) === "[object Array]";
+function isArray(obj) {
+  return Object.prototype.toString.call(obj) === "[object Array]";
 }
+
+// 通过Array内置方法isArray()判断是否为数组，该方法 返回一个布尔值
+
+Array.isArray(obj)
+
+// 通过instanceof判断是否为数组，但是instanceof在跨frame对象构建的场景下会失败或者说是在多iframe/window环境下，通过instanceof来识别对象是否为数组会出错
+obj instanceof Array
